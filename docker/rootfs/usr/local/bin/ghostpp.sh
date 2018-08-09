870fd166f023889910ee02542695620d8de966a7
Philipp Heuer <git@philippheuer.me>
2018-08-09 17:46:40 +0200

    chore: copy latest .j scripts into mapcfgs on start

--- docker/rootfs/usr/local/bin/ghostpp.sh
+++ docker/rootfs/usr/local/bin/ghostpp.sh
@@ -7,6 +7,10 @@ export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 # create environment based configuration
 envsubst < /opt/ghostpp/docker.cfg > /opt/ghostpp/ghost.cfg
 
+# copy files
+cp /opt/ghostpp/war3/scripts/blizzard.j /opt/ghostpp/data/mapcfgs
+cp /opt/ghostpp/war3/scripts/common.j /opt/ghostpp/data/mapcfgs
+
 # start bot
 cd /opt/ghostpp
 ./ghost++
