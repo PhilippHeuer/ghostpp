80641b560aa0bfe3770f5cb6c9562efd9e8f2315
Philipp Heuer <git@philippheuer.me>
2018-08-10 15:46:43 +0200

    chore: create initial directories on startup

--- docker/rootfs/usr/local/bin/ghostpp.sh
+++ docker/rootfs/usr/local/bin/ghostpp.sh
@@ -7,9 +7,11 @@ export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 # create environment based configuration
 envsubst < /opt/ghostpp/docker.cfg > /opt/ghostpp/ghost.cfg
 
+# create directories
+mkdir -p /opt/ghostpp/data/mapcfgs /opt/ghostpp/data/maps /opt/ghostpp/data/savegames /opt/ghostpp/data/replays
 # copy files
-cp /opt/ghostpp/war3/scripts/blizzard.j /opt/ghostpp/data/mapcfgs
-cp /opt/ghostpp/war3/scripts/common.j /opt/ghostpp/data/mapcfgs
+cp /opt/ghostpp/war3/scripts/blizzard.j /opt/ghostpp/data/mapcfgs/blizzard.j
+cp /opt/ghostpp/war3/scripts/common.j /opt/ghostpp/data/mapcfgs/common.j
 
 # start bot
 cd /opt/ghostpp
